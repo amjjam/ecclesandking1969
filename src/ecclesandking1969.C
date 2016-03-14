388f32e18bd7c300a3c7899eb8c4c55805f72a0d
Anders M. Jorgensen <andersmjorgensen.nmt@gmail.com>
2016-03-14 00:28:40 -0600

    Added capability to change the amplitude of the Gaussians in the model and to add an offset.

--- src/ecclesandking1969.C
+++ src/ecclesandking1969.C
@@ -8,16 +8,11 @@
 #include "../include/ecclesandking1969.H"
 
 /*=============================================================================
-  ECCLESANDKING1969() - constructor
+  ECCLESANDKING1969(double A, double B) - constructor
+
   ============================================================================*/
-ECCLESANDKING1969::ECCLESANDKING1969(){
-  a1=12.55e5;
-  b1=8.35;
-  c1=7.99;
-  a2=12.55e5;
-  b2=8.36;
-  c2=7.99;
-  k=1;
+ECCLESANDKING1969::ECCLESANDKING1969(double A, double B){
+  setParams(A,B);
 }
 
 
@@ -30,6 +25,24 @@ ECCLESANDKING1969::~ECCLESANDKING1969(){
 
 
 /*=============================================================================
+  void setParams(double A, double B) - set parameters
+
+  double A - the amplitude of the two Gaussians (default is 12.55e5)
+  double B - the offset, constant on which the Gaussians sit (default is 0)
+  ============================================================================*/
+void ECCLESANDKING1969::setParams(double A, double B){
+  a1=A;
+  b1=8.35;
+  c1=7.99;
+  a2=A;
+  b2=8.36;
+  c2=7.99;
+  k=1;
+  d=B;
+}
+
+
+/*=============================================================================
   double getDensity(double phim) - return density in electrons per cm^-3
 
   double phim - magnetic latitude in degrees
@@ -37,6 +50,5 @@ ECCLESANDKING1969::~ECCLESANDKING1969(){
 double ECCLESANDKING1969::getDensity(double phim){
   double dphi1=(phim-b1);
   double dphi2=(phim+b2);
-  return a1/k*exp(-dphi1*dphi1/c1/c1)+a2/k*exp(-dphi2*dphi2/c2/c2);
+  return a1/k*exp(-dphi1*dphi1/c1/c1)+a2/k*exp(-dphi2*dphi2/c2/c2)+d;
 }
-
